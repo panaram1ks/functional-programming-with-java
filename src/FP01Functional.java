158ff69d961e1a918035b81ba0bcd12b6ec988de
parom <parom.evgen@mail.ru>
2023-04-27 22:00:23 +0300

    use filter

--- src/FP01Functional.java
+++ src/FP01Functional.java
@@ -4,16 +4,21 @@ public class FP01Functional {
 
     public static void main(String[] args) {
 
-        printAllNumbersInListFunctional(List.of(12, 9 ,13, 4, 6, 2, 4, 12 ,15));
+        List<Integer> integers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+//        printAllNumbersInListFunctional(integers);
+        printEvenNumbersInListFunctional(integers);
     }
-
-    private static void print(int number){
-        System.out.println(number);
-    }
-
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
         //What to do?
-        numbers.stream().forEach(FP01Functional::print);//Method reference
+        numbers.stream().forEach(System.out::println);//Method reference
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream().filter(FP01Functional::isEven).forEach(System.out::println);//Method reference
     }
 }
--- src/FP01Structured.java
+++ src/FP01Structured.java
@@ -4,14 +4,28 @@ public class FP01Structured {
 
     public static void main(String[] args) {
 
-        printAllNumbersInListStructured(List.of(12, 9 ,13, 4, 6, 2, 4, 12 ,15));
+        List<Integer> integers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        //printAllNumbersInListStructured(integers);
+        printEvenNumbersInListStructured(integers);
     }
 
 
     private static void printAllNumbersInListStructured(List<Integer> numbers) {
         //How to loop the numbers?
-        for(int number: numbers){
+        for (int number : numbers) {
             System.out.println(number);
         }
     }
+
+    private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+        //How to loop the numbers?
+        for (int number : numbers) {
+            //if(even)
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+    }
+
+
 }
